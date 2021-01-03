0e684c92dffc24ac703d29cb787038c821be2a3d
Javier Paredes <fjparedesb@gmail.com>
2021-01-03 16:46:13 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // / PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+// ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// DB
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:test-123456@cluster0.rp3yb.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.urlDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,38 +1,31 @@
 require('./config/config');
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-    //const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const bodyParser = require('body-parser');
+//const port = 3000
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario!')
-})
+//conexion a la bd
+mongoose.connect(process.env.urlDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-app.post('/usuario', (req, res) => {
+        if (err) throw err;
 
-    let body = req.body;
+        console.log('BASE DE DATOS ONLINE');
 
-    res.json({ persona: body })
-})
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-
-    res.json({ id });
-})
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario!')
-})
+    });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
